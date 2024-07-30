5064af2dda74afc9f7137990d79b39100009bb49
Sagar Kumar sah Kanu <sagarsah20834@gmail.com>
2024-07-30 17:16:38 +0545

    Frontend Issue

--- src/Components/Forms/Login.jsx
+++ src/Components/Forms/Login.jsx
@@ -84,6 +84,9 @@ const Login = () => {
               <button
                 className="inline-block py-3 px-7 mb-6 w-full text-base text-green-50 font-medium text-center leading-6 bg-green-500 hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md shadow-sm"
                 type="submit"
+                onClick={() => {
+                  loginUserAction(formData);
+                }}
               >
                 Sign In
               </button>
